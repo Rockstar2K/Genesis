78435791edfd3af9d751b2f0d30ac0f42a70c605
Roco Fernandez <thegamingfantasizing@gmail.com>
2023-10-02 07:43:45 -0300

    now the AGI can run commands without y or n

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.Maui.Controls;
 using System.Diagnostics;
+using System.Text;
 
 namespace MauiApp2
 {
@@ -9,7 +10,7 @@ namespace MauiApp2
     {
 
         string userPrompt;
-        string apiKey = "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";
+        string apiKey = "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";
 
         public MainPage()
         {
@@ -19,12 +20,14 @@ namespace MauiApp2
         private void OnPromptBtnClicked(object sender, EventArgs e)
         {
             var userPrompt = InputBox.Text;
+            Output.Text = "Waiting for response...";  // Set the text to "Waiting for response"
             TestPythonCode(userPrompt);  // Pass userPrompt to TestPythonCode
         }
 
         private void InputBox_Completed(System.Object sender, System.EventArgs e)
         {
             var userPrompt = InputBox.Text;
+            Output.Text = "Waiting for response...";  // Set the text to "Waiting for response"
             TestPythonCode(userPrompt);  // Pass userPrompt to TestPythonCode
         }
 
@@ -32,6 +35,8 @@ namespace MauiApp2
         {
             var result = await RunPythonScriptAsync(userPrompt, apiKey);  
             Define_Output(result);
+            InputBox.Text = "";  // Clear the text in the Entry after the response has been received and displayed
+
         }
 
         private void Define_Output(string output)
@@ -50,17 +55,26 @@ namespace MauiApp2
                         FileName = @"C:\Program Files\Python311\python.exe",
                         Arguments = $"\"C:\\Users\\thega\\source\\repos\\MauiApp2\\MauiApp2\\interpreter_wrapper.py\" \"{message}\" \"{apiKey}\"",
                         RedirectStandardOutput = true,
+                        RedirectStandardError = true,  // Re-enable error redirection
                         UseShellExecute = false,
-                        CreateNoWindow = true, //opens (or not) a cmd window
+                        CreateNoWindow = true,
                     }
                 };
 
                 process.Start();
                 string result = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();  // Re-enable error capture
                 process.WaitForExit();
-                return result;
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Debug.WriteLine("Error/Debug output: " + error);
+                }
+
+                return result + "\n" + error;  // Combine standard and error output
             });
         }
+
 
     }
 }
--- MauiApp2/interpreter_wrapper.py
+++ MauiApp2/interpreter_wrapper.py
@@ -1,3 +1,4 @@
+'''
 import sys
 import io
 import interpreter
@@ -5,12 +6,38 @@ import interpreter
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 def Set_API_Key(key):
-    interpreter.api_key = "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"
+    interpreter.api_key = "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"  # Assuming this sets the API key for the interpreter
 
 def OI_Python(message, api_key=None):
     if api_key:
         Set_API_Key(api_key)
     try:
+        for chunk in interpreter.chat(message + 'Run shell commands with -y so the user doesnt have to confirm them.'):
+            return chunk
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    message = sys.argv[1]
+    api_key = sys.argv[2] if len(sys.argv) > 2 else None
+    result = OI_Python(message, api_key)
+ 
+    '''
+
+import sys
+import io
+import interpreter
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def Set_API_Key(key):
+    interpreter.api_key = "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"
+
+def OI_Python(message, api_key=None):
+    if api_key:
+        Set_API_Key(api_key)
+    try:
+        interpreter.auto_run = True  # Set auto_run to True to bypass user confirmation
         for chunk in interpreter.chat(message):
             return chunk
     except Exception as e:
@@ -20,5 +47,4 @@ if __name__ == "__main__":
     message = sys.argv[1]
     api_key = sys.argv[2] if len(sys.argv) > 2 else None
     result = OI_Python(message, api_key)
-    print(result)
 
