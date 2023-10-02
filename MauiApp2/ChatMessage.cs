39a96cdafc27bf8f97d3902a392b710896d5e17e
Roco Fernandez <thegamingfantasizing@gmail.com>
2023-10-02 17:25:57 -0300

    AGI now has a nicer UI

---
+++ MauiApp2/ChatMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2
+{
+    public class ChatMessage
+    {
+
+       public string Text { get; set; }
+       public bool IsUser { get; set; }  // true if this message is from the user, false if from the bot
+
+    }
+}
