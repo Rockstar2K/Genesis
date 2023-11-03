7638703e2758ba56a8e277d9b2cec8c2109ff536
NatanAmado <124468759+NatanAmado@users.noreply.github.com>
2023-11-04 00:17:13 +0100

    Finished Python + OI instalation

--- MauiApp2/Welcome_Page.xaml.cs
+++ MauiApp2/Welcome_Page.xaml.cs
@@ -7,10 +7,11 @@ namespace MauiApp2;
 public partial class Welcome_Page : ContentPage
 {
     private double progressValue;
-    private const string PythonInstallerUrlWindows = "https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe";
-    private const string PythonInstallerUrlMac = "https://www.python.org/ftp/python/3.9.7/python-3.9.7-macosx10.9.pkg";
-    private const string PythonInstallerNameWindows = "python-3.9.7-amd64.exe";
-    private const string PythonInstallerNameMac = "python-3.9.7-macosx10.9.pkg";
+    private const string PythonInstallerUrlWindows = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe";
+    private const string PythonInstallerUrlMac = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-macos11.pkg";
+
+    private const string PythonInstallerNameWindows = "python-3.12.0-amd64.exe";
+    private const string PythonInstallerNameMac = "python-3.12.0-macosx11.pkg";
 
     public Welcome_Page()
 	{
@@ -69,7 +70,16 @@ public partial class Welcome_Page : ContentPage
 
             await DisplayAlert("Download Complete", "Python installer downloaded successfully.", "OK");
             InstallPython(installerName);
-            await DisplayAlert("Installation Complete", "Python has been installed successfully.", "OK");
+            string pythonPath = GetPythonInstallationPath();
+            await DisplayAlert("Python Path", $"Python was installed at: {pythonPath}", "OK");
+
+            //Update pip
+            await UpgradePipAsync(pythonPath);
+
+            // After Python installation is complete, install open-interpreter
+            await InstallPythonLibrary("open-interpreter");
+            progressBar.Progress = 1;
+            await DisplayAlert("Installation Complete", "Open Interpreterhas been installed successfully.", "OK");
         }
         catch (Exception ex)
         {
@@ -88,7 +98,7 @@ public partial class Welcome_Page : ContentPage
             totalRead += read;
             if (totalLength != -1)
             {
-                var progress = (int)((totalRead * 100) / totalLength);
+                var progress = (int)((totalRead * 90) / totalLength);
                 progressBar.Progress = progress / 100.0;
             }
         }
@@ -121,6 +131,107 @@ public partial class Welcome_Page : ContentPage
         }
         process.Start();
         process.WaitForExit();
+
+
     }
+
+    private async Task InstallPythonLibrary(string libraryName)
+    {
+        string pythonPath = GetPythonInstallationPath();
+        Preferences.Set("Python_Path", pythonPath); 
+        if (string.IsNullOrEmpty(pythonPath))
+        {
+            await DisplayAlert("Error", "Failed to find Python installation path.", "OK");
+            return;
+        }
+
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = pythonPath,
+                Arguments = $"-m pip install {libraryName}",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+        process.Start();
+        await process.WaitForExitAsync();
+    }
+
+    private async Task UpgradePipAsync(string pythonPath)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = pythonPath,
+                Arguments = "-m pip install --upgrade pip",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+
+        // Optionally, read the output to capture any messages
+        string output = await process.StandardOutput.ReadToEndAsync();
+
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            // There was an error, handle it as needed
+            await DisplayAlert("Error", "Failed to upgrade pip.", "OK");
+        }
+        else
+        {
+            // pip was upgraded successfully
+            await DisplayAlert("Success", "pip was upgraded successfully.", "OK");
+        }
+    }
+
+
+    private string GetPythonInstallationPath()
+    {
+        string[] possibleExecutables = { "python", "python3" };
+        foreach (var executable in possibleExecutables)
+        {
+            var process = new Process();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd",
+                    Arguments = $"/C where {executable}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                };
+            }
+            else  // macOS and Linux
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "which",
+                    Arguments = executable,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                };
+            }
+            process.Start();
+            string pythonPath = process.StandardOutput.ReadLine();
+            process.WaitForExit();
+            if (!string.IsNullOrEmpty(pythonPath))
+            {
+                return pythonPath;
+            }
+        }
+        return null;
+    }
+
 
 }
